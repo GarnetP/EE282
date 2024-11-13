1346acbe4dccb81c768e1098dd9a3d7331d15479
GarnetP <gphinney@uci.edu>
2024-11-13 14:14:15 -0800

    worked on HW3

--- code/scripts/hw3_genome_summary.sh
+++ code/scripts/hw3_genome_summary.sh
@@ -1,6 +1,7 @@
 # Script for genome summary
 
 # Verify file integrity 
+echo "File integrity check:"
 cd ~/myrepos/ee282/data/
 md5sum dmel-all-chromosome-r6.48.fasta.gz > dmel-all-chromosome-r6.48.fasta.gz.$
 md5sum -c dmel-all-chromosome-r6.48.fasta.gz.md5
@@ -8,5 +9,6 @@ md5sum dmel-all-chromosome-r6.48.fasta.gz
 
 
 # Process the fasta file 
+echo "Genome summary:"
 module load ucsc-tools/v429
 faSize -tab dmel-all-chromosome-r6.48.fasta.gz | grep -e "baseCount" -e "nBaseCount" -e "seqCount"
